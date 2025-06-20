8caa4305c2b75520e9baeb3b39e1d9fc61440807
Francis Kornu <fkay0450@gmail.com>
2025-06-20 21:12:26 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import ProtectedRoute from "./utils/protectedRoutes"
+
+const AuthPage = React.lazy(()=> import('./pages/authentication/auth'));
+const RegisterPage = React.lazy(()=> import('./pages/authentication/register'));
+const HomePage = React.lazy(()=> import('./pages/hustler/home'));
+const RegisterDetailsPage = React.lazy(()=> import('./pages/authentication/register_details'));
+const LoginPage = React.lazy(()=> import('./pages/authentication/login'));
+const ConfirmationPage = React.lazy(()=> import('./pages/authentication/confirmation'));
+const MyHustlesPage = React.lazy(()=> import('./pages/hustler/myHustles'))
+const SettingsPage = React.lazy(() => import('./pages/hustler/settings'))
+const MyWalletPage = React.lazy(() => import('./pages/hustler/myWallet'))
+const CreatorHomePage = React.lazy(() => import('./pages/creator/home'))
+const MyCreatorHustlesPage = React.lazy(() => import('./pages/creator/myHustles'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Suspense fallback={"...loading"}>
+      <Routes>
+        {/* <Route element={<ProtectedRoute />}> */}
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/myHustles" element={<MyHustlesPage/>}/>
+          <Route path="/settings" element={<SettingsPage/>}/>
+          <Route path="/myWallet" element={<MyWalletPage/>}/>
+
+          <Route path="/creator/home" element={<CreatorHomePage/>}/>
+          <Route path="/creator/hustles" element={<MyCreatorHustlesPage/>}/>
+        {/* </Route> */}
+        <Route path="/auth" element={<AuthPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/register_details" element={<RegisterDetailsPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/confirmation" element={<ConfirmationPage/>}/>
+      </Routes>
+    </React.Suspense>
   );
 }
 
